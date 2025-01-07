991f744a469d1c2eec4c417b1b00e4a5cc184e04
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-07 21:26:07 +0700

    Security

---
+++ Security.sql
@@ -0,0 +1,24 @@
+﻿use master 
+go 
+create login DB_USER
+	with password = N'123',
+	check_expiration = off,
+	check_policy = off
+go
+
+use DiaLy
+go
+create user DB_USER for login DB_USER;
+
+ALTER ROLE db_datareader ADD MEMBER DB_USER;  -- Cho phép đọc dữ liệu
+ALTER ROLE db_datawriter ADD MEMBER DB_USER; -- Cho phép ghi dữ liệu
+
+GRANT SELECT ON [dbo].[TINH_TP] TO DB_USER;
+
+--Thu hoi
+REVOKE SELECT ON [dbo].[TINH_TP] FROM DB_USER;
+
+--Tu choi quyen 
+DENY SELECT ON [dbo].[TINH_TP] TO DB_USER;
+
+
